007acb56118f16e8c11db8b7480dc9bcd3c4d428
Levi_o <632891553@qq.com>
2022-01-24 13:29:22 +0800

    add drei

--- src/routers/spacePage.js
+++ src/routers/spacePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
+import { Sky, PointerLockControls } from "@react-three/drei";
 import { Ground } from "../components/Ground";
 import { RotatingCube } from "../components/RotatingCube";
 import "../css/spacePage.css";
@@ -11,9 +12,10 @@ class Space extends React.Component {
     return (
       <Canvas
         gl={{ alpha: false }}
-        camera={{ position: [0, 0, 10], fov: 45 }}
+        camera={{position:[0,2,-10], fov: 45 }}
         dpr={window.devicePixelRatio}
       >
+        <Sky sunPosition={[100, 20, 100]} />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Physics gravity={[0, -30, 0]}>
@@ -22,6 +24,7 @@ class Space extends React.Component {
           <RotatingCube position={[1.2, 0, 0]} />
           <RotatingCube position={[0, 1, -1]} />
         </Physics>
+        <PointerLockControls />
       </Canvas>
     );
   }
